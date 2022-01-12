663088f0f8b73d5da2af2ebff49a43983308f738
robmikh <rob.mikh@outlook.com>
2022-01-12 00:59:44 -0800

    run fmt

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ fn main() {
     let word_data = include_str!("../data/words.txt");
     let words: Vec<&str> = word_data.lines().collect();
     let words = remove_words_with_chars(words, &['w', 't', 'p', 's', 'g', 'l', 'c', 'n']);
-    let words = remove_words_without_chars(words, &['r',]);
+    let words = remove_words_without_chars(words, &['r']);
     let words = filter_words_with_char_and_position(words, 'a', 1);
     let words = filter_words_with_char_and_position(words, 'o', 3);
     println!("Found {} word(s):", words.len());
@@ -11,7 +11,11 @@ fn main() {
     }
 }
 
-fn filter_words_with_char_and_position<'a>(words: Vec<&'a str>, letter: char, position: usize) -> Vec<&'a str> {
+fn filter_words_with_char_and_position<'a>(
+    words: Vec<&'a str>,
+    letter: char,
+    position: usize,
+) -> Vec<&'a str> {
     let mut filtered_words = Vec::new();
     for word in words {
         if word.chars().nth(position).unwrap() == letter {
